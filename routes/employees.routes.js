7f97dfcb3263a23443e81a89e645a899dc46d039
Wojciech Dlugosz <webmaster@styronet.pl>
2024-11-04 12:32:26 +0100

    Modify rest of endpoints to connect with mongodb, delete db.js

--- routes/employees.routes.js
+++ routes/employees.routes.js
@@ -1,34 +1,76 @@
 const express = require('express');
 const router = express.Router();
-const db = require('./../db');
+const ObjectId = require('mongodb').ObjectId;
 
 router.get('/employees', (req, res) => {
-  res.json(db.employees);
+  req.db.collection('employees')
+    .find()
+    .toArray()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err });
+    });
 });
 
 router.get('/employees/random', (req, res) => {
-  res.json(db.employees[Math.floor(Math.random() * db.length)]);
+  req.db.collection('employees')
+    .aggregate([{ $sample: { size: 1 } }])
+    .toArray()
+    .then((data) => {
+      res.json(data[0]);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err });
+    });
 });
 
 router.get('/employees/:id', (req, res) => {
-  res.json(db.employees.find(item => item.id == req.params.id));
+  req.db.collection('employees')
+    .findOne({ _id: ObjectId(req.params.id) })
+    .then((data) => {
+      if(!data) res.status(404).json({ message: 'Not found' });
+      else res.json(data);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err });
+    });
 });
 
 router.post('/employees', (req, res) => {
   const { firstName, lastName } = req.body;
-  db.employees.push({ id: 3, firstName, lastName })
-  res.json({ message: 'OK' });
+  req.db.collection('employees')
+    .insertOne({ firstName: firstName, lastName: lastName })
+    .then(() => {
+      res.json({ message: 'OK' });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err });
+    });
 });
 
 router.put('/employees/:id', (req, res) => {
   const { firstName, lastName } = req.body;
-  db = db.employees.map(item => (item.id == req.params.id) ? { ...item, firstName, lastName } : item );
-  res.json({ message: 'OK' });
+  req.db.collection('employees')
+    .updateOne({ _id: ObjectId(req.params.id) }, { $set: { firstName: firstName, lastName: lastName } })
+    .then(() => {
+      res.json({ message: 'OK' });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err });
+    });
 });
 
 router.delete('/employees/:id', (req, res) => {
-  db = db.employees.filter(item => item.id != req.params.id)
-  res.json({ message: 'OK' });
+  req.db.collection('employees')
+    deleteOne({ _id: ObjectId(req.params.id) })
+    .then(() => {
+      res.json({ message: 'OK' });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err });
+    });
 });
 
 module.exports = router;
--- routes/products.routes.js
+++ routes/products.routes.js
@@ -1,36 +1,76 @@
-// post.routes.js
-
 const express = require('express');
 const router = express.Router();
-const db = require('./../db');
+const ObjectId = require('mongodb').ObjectId;
 
 router.get('/products', (req, res) => {
-  res.json(db.products);
+  req.db.collection('products')
+    .find()
+    .toArray()
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err });
+    });
 });
 
 router.get('/products/random', (req, res) => {
-  res.json(db.products[Math.floor(Math.random() * db.length)]);
+  req.db.collection('products')
+    .aggregate([{ $sample: { size: 1 } }])
+    .toArray()
+    .then((data) => {
+      res.json(data[0]);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err });
+    });
 });
 
 router.get('/products/:id', (req, res) => {
-  res.json(db.products.find(item => item.id == req.params.id));
+  req.db.collection('products')
+    .findOne({ _id: ObjectId(req.params.id) })
+    .then((data) => {
+      if(!data) res.status(404).json({ message: 'Not found' });
+      else res.json(data);
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err });
+    });
 });
 
 router.post('/products', (req, res) => {
   const { name, client } = req.body;
-  db.products.push({ id: 3, name, client })
-  res.json({ message: 'OK' });
+  req.db.collection('products')
+    .insertOne({ name: name, client: client })
+    .then(() => {
+      res.json({ message: 'OK' });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err });
+    });
 });
 
 router.put('/products/:id', (req, res) => {
   const { name, client } = req.body;
-  db = db.products.map(item => (item.id == req.params.id) ? { ...item, name, client } : item );
-  res.json({ message: 'OK' });
+  req.db.collection('products')
+    .updateOne({ _id: ObjectId(req.params.id) }, { $set: { name: name, client: client } })
+    .then(() => {
+      res.json({ message: 'OK' });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err });
+    });
 });
 
 router.delete('/products/:id', (req, res) => {
-  db = db.products.filter(item => item.id != req.params.id)
-  res.json({ message: 'OK' });
+  req.db.collection('products')
+    deleteOne({ _id: ObjectId(req.params.id) })
+    .then(() => {
+      res.json({ message: 'OK' });
+    })
+    .catch((err) => {
+      res.status(500).json({ message: err });
+    });
 });
 
 module.exports = router;
