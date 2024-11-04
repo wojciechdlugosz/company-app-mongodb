3b168605919660c3749fa09e0a4c4a4396b4eeba
Wojciech Dlugosz <webmaster@styronet.pl>
2024-11-04 15:41:03 +0100

    Modify departments enpoints to show modified or deleted element

--- routes/departments.routes.js
+++ routes/departments.routes.js
@@ -54,7 +54,7 @@ router.put('/departments/:id', async (req, res) => {
     if(dep) { 
       dep.name = name; 
       await dep.save();
-      res.json({ message: 'OK' });
+      res.json({ message: 'OK', dep });
     }
     else res.status(404).json({ message: 'Not found...' });
   }
@@ -68,7 +68,7 @@ router.delete('/departments/:id', async (req, res) => {
     const dep = await Department.findById(req.params.id);
     if(dep) {
       await Department.deleteOne({ _id: req.params.id });
-      res.json({ message: 'OK' });
+      res.json({ message: 'OK', dep });
     }
     else res.status(404).json({ message: 'Not found...' });
   }
