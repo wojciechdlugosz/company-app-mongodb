0e5cde8c47df5111c0186ca2c06a6c6030bb0870
Wojciech Dlugosz <webmaster@styronet.pl>
2024-11-05 14:35:26 +0100

    Use populate to connect employees collection with departments collection

--- routes/employees.routes.js
+++ routes/employees.routes.js
@@ -5,7 +5,7 @@ const Employee = require('../models/employee.model');
 
 router.get('/employees', async (req, res) => {
   try {
-    res.json(await Employee.find());
+    res.json(await Employee.find().populate('department'));
   }
   catch(err) {
     res.status(500).json({ message: err });
@@ -16,7 +16,7 @@ router.get('/employees/random', async (req, res) => {
   try {
     const count = await Employee.countDocuments();
     const rand = Math.floor(Math.random() * count);
-    const empl = await Employee.findOne().skip(rand);
+    const empl = await Employee.findOne().populate('department').skip(rand);
     if(!empl) res.status(404).json({ message: 'Not found' });
     else res.json(empl);
   }
@@ -27,7 +27,7 @@ router.get('/employees/random', async (req, res) => {
 
 router.get('/employees/:id', async (req, res) => {
   try {
-    const empl = await Employee.findById(req.params.id);
+    const empl = await Employee.findById(req.params.id).populate('department');
     if(!empl) res.status(404).json({ message: 'Not found' });
     else res.json(empl);
   }
