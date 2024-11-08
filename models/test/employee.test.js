d657ccdcedc98c2f3b0b0e148bb6e530cea21a51
Wojciech Dlugosz <webmaster@styronet.pl>
2024-11-08 12:19:35 +0100

    Add tests for employee model

---
+++ models/test/employee.test.js
@@ -0,0 +1,61 @@
+const Employee = require("../employee.model");
+const expect = require("chai").expect;
+const mongoose = require("mongoose");
+
+describe("Employee", () => {
+    it('should throw an error if no arguments', async () => {
+        const empl = new Employee({});
+        const err = empl.validateSync();
+        expect(err.errors.firstName && err.errors.lastName && err.errors.department).to.exist;
+        
+        after(() => {
+            mongoose.models = {};
+          });      
+    });
+    it('should throw an error if there is an arg missing', () => {
+        const cases = [
+            { firstName: 'Amanda', lastName: 'Doe' },
+            { firstName: 'Amanda' },
+            { lastName: 'Doe', department: 'IT' }
+        ]
+        for (let c of cases) {
+            const empl = new Employee(c)
+            const err = empl.validateSync();
+            expect(err).to.exist;
+        }
+        after(() => {
+            mongoose.models = {};
+        });
+    });
+    it('should throw an error if any of arguments is not a string', () => {
+        const cases = [
+            { firstName: 'Amanda', lastName: 'Doe', department: [] },
+            { firstName: 'Amanda', lastName: {}, department: 'IT' },
+            { firstName: [], lastName: {}, department: [] },
+        ]
+        
+        for (let c of cases) {
+            const empl = new Employee(c)
+            const err = empl.validateSync();
+            expect(err).to.exist;
+        }
+        after(() => {
+            mongoose.models = {};
+        });
+    });
+    it('should NOT throw an error if all of arguments are OK', () => {
+        const cases = [
+            { firstName: 'Amanda', lastName: 'Doe', department: 'IT' },
+            { firstName: 'John', lastName: 'Doe', department: 'Marketing' }
+        ]
+        
+        for (let c of cases) {
+            const empl = new Employee(c)
+            const err = empl.validateSync();
+            expect(err).to.not.exist;
+        }
+        after(() => {
+            mongoose.models = {};
+        });
+    });
+});
